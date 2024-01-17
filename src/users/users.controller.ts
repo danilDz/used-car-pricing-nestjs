e468066abbfc2d69d724d3f8482653c3a55d3f34
daniildzk <freekick2017uk@gmail.com>
2024-01-17 16:15:54 +0200

    Add custom Data Serialization logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,8 +12,11 @@ import {
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { UsersService } from "./users.service";
+import { Serialize } from "src/interceptors/serialize.interceptor";
+import { UserDto } from "./dto/user.dto";
 
 @Controller("auth")
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
